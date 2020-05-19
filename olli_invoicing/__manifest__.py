731bcd77c45e6aa5fac72b1b560909aede2b765d
maxwellkimutai <maxwellkimutai@gmail.com>
2020-05-19 17:51:19 +0300

    add public create invoice method to call private create invoice method

--- olli_invoicing/__manifest__.py
+++ olli_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "Olli Invoicing",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,13 +10,13 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Jenga Tech Limited",
+    'website': "https://odoo-erp-jenga.com/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Invoicing',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
