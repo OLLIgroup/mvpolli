4ad8488a834f0d426bb8802617ae3f951663772a
maxwellkimutai <maxwellkimutai@gmail.com>
2020-05-20 15:37:06 +0300

    add logging

--- olli_invoicing/models/sale.py
+++ olli_invoicing/models/sale.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def create_invoices(self):
+
+        _logger.info(_(self._create_invoices()))
+
         return self._create_invoices()
