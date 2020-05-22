67463a149b8e9d4a481a7525e3dfeea48741c9d1
maxwellkimutai <maxwellkimutai@gmail.com>
2020-05-22 16:40:29 +0300

    remove function

--- olli_invoicing/models/sale.py
+++ olli_invoicing/models/sale.py
@@ -19,28 +19,5 @@ class SaleOrder(models.Model):
         })
         payment.create_payments()
         template = self.env.ref('account.email_template_edi_invoice')
-        # template_id.send_mail(invoice_id, force_send=True)
-        composer = self.env['mail.compose.message'].create({
-            'composition_mode': 'comment',
-        })
-        invoice_send = self.env['account.invoice.send'].create({
-            'is_email': True,
-            'invoice_ids': [invoice_id],
-            'composer_id': composer.id,
-            'template_id': template.id
-        })
-        lang = get_lang(self.env)
-        ctx = dict(
-            default_model='account.move',
-            default_res_id=invoice_id,
-            default_use_template=bool(template),
-            default_template_id=template and template.id or False,
-            default_composition_mode='comment',
-            mark_invoice_as_sent=True,
-            custom_layout="mail.mail_notification_paynow",
-            model_description=self.with_context(lang=lang).type_name,
-            force_email=True
-        )
-        invoice_send = invoice_send.with_context(ctx)
-        invoice_send.send_and_print_action()
+        template.send_mail(invoice_id, force_send=True)
         return invoice_id
